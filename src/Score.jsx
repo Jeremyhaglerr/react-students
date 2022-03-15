ad741fba8c9af9a97d3c6904621ba5e21b5b5082
Jeremy <jeremyhaglerr@gmail.com>
2022-03-15 15:40:30 -0500

    add classNames to support bootstap integration

--- src/Score.jsx
+++ src/Score.jsx
@@ -1,11 +1,13 @@
 const Score = (props) => {
   return ( 
-    props.student.scores.map((result) => 
-      <div className="score" >
+    <ul className="list-group list-group-flush" >
+    {props.student.scores.map((result) => 
+      <li className="score list-group-item" key={result.score} >
         <p> 
-          <span className="title" > Score:</span> 
-          {result.score} <span className="date" > - {result.date}</span></p> </div>
-    )
+          <span className="title" >Score:</span> 
+         {result.score} <span className="date" > - {result.date}</span></p> </li>
+    )}
+    </ul>
    );
 }
  
--- src/Student.jsx
+++ src/Student.jsx
@@ -3,9 +3,9 @@ import './App.css'
 
 const Student = (props) => {
   return ( props.students.map((student,idx) =>
-    <div className="student" >
-      <p className="name" > <span className="title" > Name:</span> {student.name} </p> 
-      <p> <span className="title" > Bio: </span>{student.bio}</p>
+    <div className="student card card-body" key={student.name} >
+      <p className="name card-header" > <span className="title" > Name:</span> {student.name} </p> 
+      <p className="card-subtitle mb-2" > <span className="title" > Bio:</span>{student.bio}</p>
       <Score student={student}/>
     </div>
     )
